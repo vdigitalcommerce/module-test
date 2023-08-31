d25e3f46510bfe8ee980568cdc2031d11a112a2f
Aditya <aditya@vdcstore.com>
2023-08-31 18:16:23 +0530

    1.0.0

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © VdigitalCommerce_Test All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'VdigitalCommerce_Test', __DIR__);
+
